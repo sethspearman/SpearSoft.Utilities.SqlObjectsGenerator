c77364da7d9761d72217f305c533dd0fea5ec45c
Seth Spearman <sethspearman@gmail.com>
2016-08-13 18:53:07 -0400

    class added for search replace function

---
+++ SearchReplaceString.cs
@@ -0,0 +1,8 @@
+﻿namespace SpearSoft.Utilities.SqlObjectsGenerator
+{
+    public class SearchReplaceString
+    {
+        public string SearchToken { get; set; }
+        public string ReplaceValue { get; set; }
+    }
+}
